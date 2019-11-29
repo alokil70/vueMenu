6e9b26895898e022d1d548fa0cf51930b21ecc8a
Zero <alokil70@gmail.com>
2019-11-29 09:33:32 +0300

    yarn install

--- src/main/java/ru/restoran/menu/controller/MessageController.java
+++ src/main/java/ru/restoran/menu/controller/MessageController.java
@@ -1,7 +1,41 @@
 package ru.restoran.menu.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.restoran.menu.exeptions.NotFoundExeption;
 
-@Controller
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
 public class MessageController {
+
+    private int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>() {{
+            put("id", "1");
+            put("text", "first message");
+        }});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> getById(@PathVariable String id){
+        return messages.stream().filter(message -> message.get("id").equals(id))
+                .findFirst().orElseThrow(NotFoundExeption::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message){
+        message.put("id", String.valueOf(counter++));
+        messages.add(message);
+        return (Map<String, String>) messages;
+    }
 }
---
+++ src/main/java/ru/restoran/menu/exeptions/NotFoundExeption.java
@@ -0,0 +1,9 @@
+package ru.restoran.menu.exeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundExeption extends RuntimeException {
+
+}
