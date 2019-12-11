ebe6133410ac56e50e143642792c1dc4be759a36
Zero <alokil70@gmail.com>
2019-12-11 16:34:49 +0300

    vuetify started

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
+//import Vuetify from "vuetify/lib"
+import vuetify from "./plugins/vuetify"
 import VueResource from 'vue-resource'
 import App from './pages/App.vue'
-//import Vuetify from "vuetify/lib"
-import 'vuetify/dist/vuetify.min.css'
+//import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(VueResource)
 //Vue.use(Vuetify)
+Vue.use(VueResource)
 
 new Vue({
-    el: '#app',
-    render: a => a(App)
-})
+  vuetify,
+    render: h => h(App)
+}).$mount('#app')
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
     mode: 'development',
@@ -32,16 +33,34 @@ module.exports = {
                 loader: 'vue-loader'
             },
             {
-                test: /\.css$/,
+                test: /\.s(c|a)ss$/,
                 use: [
                     'vue-style-loader',
-                    'css-loader'
-                ]
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        // Requires sass-loader@^7.0.0
+                        options: {
+                            implementation: require('sass'),
+                            fiber: require('fibers'),
+                            indentedSyntax: true // optional
+                        },
+                        // Requires sass-loader@^8.0.0
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: require('fibers'),
+                                indentedSyntax: true // optional
+                            },
+                        },
+                    },
+                ],
             }
         ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new VuetifyLoaderPlugin()
     ],
     resolve: {
         modules: [
