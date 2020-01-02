35168a652e3eab887d7a413c9c60c25beb7e7503
Zero <alokil70@gmail.com>
2020-01-02 23:21:12 +0300

    vuex configured

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,10 +4,8 @@ import '@babel/polyfill'
 import 'api/resource'
 import App from 'App.vue'
 import store from 'store/index'
-import VueResource from 'vue-resource'
 import router from 'router/index'
 
-Vue.use(VueResource);
 
 new Vue({
 //  el: '#app',
